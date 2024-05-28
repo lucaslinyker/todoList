ba67c3d82e615013ae7a38898bd121a1992f624a
lucaslinyker <lucaslinyker@outlook.com>
2024-05-28 15:25:50 -0300

    :lipstick: feat: Criar estilização do formulário

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,12 @@
 import { PlusCircle } from '@phosphor-icons/react'
 
+import styles from './Form.module.css'
+
 export function Form() {
   return (
-    <form>
-      <input type="text" placeholder='Adicione uma nova tarefa' />
-      <button type="submit">
+    <form className={styles.form}>
+      <input type="text" placeholder='Adicione uma nova tarefa' className={styles.input} />
+      <button type="submit" className={styles.button}>
         Criar
         <PlusCircle size={16} />
       </button>
