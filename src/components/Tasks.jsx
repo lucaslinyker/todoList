ae0bb272c3b390b1aaadb8e881a0ce3b8dcf3ef0
lucaslinyker <lucaslinyker@outlook.com>
2024-06-15 13:23:33 -0300

    :sparkles: feat: Tornar os dados do cabeçalho dinâmico
    Número de tasks criadas, e número de tasks concluídas

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,17 +2,19 @@ import Clipboard from '/clipboard.svg'
 
 import styles from './Tasks.module.css'
 
-export function Tasks() {
+export function Tasks({ tasks }) {
   return (
     <section className={styles.section}>
       <div className={styles.header}>
         <div className={styles.tasks}>
           <p>Tarefas criadas</p>
-          <span className={styles.span}>0</span>
+          <span className={styles.span}>{tasks.length}</span>
         </div>
         <div className={styles.completed}>
           <p>Concluídas</p>
-          <span className={styles.span}>0</span>
+          <span className={styles.span}>
+            {tasks.filter(task => task.completed === true).length}
+          </span>
         </div>
       </div>
       <div className={styles.empty}>
